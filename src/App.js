c0f11e25461a2b09abd46733e56b9bd86bbfcf24
Clemente Sepulveda <csepulveda@bzero.cl>
2025-06-29 00:10:25 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,88 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import LibraryShower from "./Library/Library";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <CardCounter>
+      <div style={{ width: '100vw', height: '100vh', backgroundColor: 'ble' }}>
+        <LibraryShower />
+
+        <Frame>
+          <Picture url="/images/000.jpg" />
+        </Frame>
+      </div>
+    </CardCounter>
+  );
+}
+
+const Frame = ({ children }) => {
+  return (
+    <div style={{
+      width: '100%',
+      height: '100%',
+      backgroundColor: 're',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      position: 'relative'
+    }}>
+      <div style={{ width: '80%', height: '80%', backgroundColor: 'gren' }}>
+        <div style={{ transform: 'rotate(0deg)' }}>
+          {children}
+        </div>
+      </div>
+
     </div>
   );
 }
 
-export default App;
+const Picture = ({ url }) => {
+  return (
+    <div style={{ width: '100px', height: '100px', backgroundColor: 're' }}>
+      <img src={url} alt="image" style={{ width: '100%', height: '100%', objectFit: 'cover' }} />
+    </div>
+  );
+}
+
+const CardCounter = ({ children }) => {
+  const updateLocalStorageCards = () => {
+    const cards = localStorage.getItem('cards');
+    const cardsArray = JSON.parse(cards);
+    cardsArray.push(obtainNewCard());
+    localStorage.setItem('cards', JSON.stringify(cardsArray));
+
+  }
+
+  const obtainNewCard = () => {
+    // TODO
+    return { id: '3', url: '/images/002.jpg', date: '2025-01-01', description: 'DEBUG 3' };
+  }
+
+  const updateLastDate = () => {
+    const lastDate = new Date().toLocaleDateString('en-US');
+    localStorage.setItem('last-date-open', lastDate);
+  }
+
+
+  useEffect(() => {
+    const lastDate = localStorage.getItem('last-date-open');
+
+    if (!lastDate) {
+      updateLocalStorageCards();
+      updateLastDate();
+    }
+
+    const currentDate = new Date().toLocaleDateString('en-US');
+
+    if (currentDate !== lastDate) {
+      updateLocalStorageCards();
+      updateLastDate();
+    }
+  }, []);
+
+  return (
+    <>
+      {children}
+    </>
+  );
+};
---
+++ src/Library/Library.js
@@ -0,0 +1,73 @@
+import React, { useEffect, useMemo, useState } from "react";
+import './Library.css';
+
+export default function LibraryShower() {
+    const [showLibrary, setShowLibrary] = useState(false);
+    const library = useMemo(() => <Library />, []);
+
+    return (
+        <>
+            <div className={`library-button`}>
+                <div className={`library-button-icon ${showLibrary ? 'library-down' : ''}`}>
+                    <svg 
+                        onClick={() => setShowLibrary(!showLibrary)} width="100%" height="100%" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M9 18L15 12L9 6" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                    </svg>
+                </div>
+                {/* <button onClick={() => setShowLibrary(!showLibrary)}>
+                    {showLibrary ? 'Hide Library' : 'Show Library'}
+                </button> */}
+            </div>
+
+            {/* {showLibrary && (
+                <div
+                    className={`library-container ${showLibrary ? 'show' : ''}`}
+                >
+                    {library}
+                </div>
+            )} */}
+
+            <div className={`library-container ${showLibrary ? 'show' : ''}`}>
+                {library}
+            </div>
+        </>
+    );
+}
+
+const Library = () => {
+    const [cards, setCards] = useState([]);
+
+    useEffect(() => {
+        const storageCards = localStorage.getItem('cards');
+        if (!storageCards) {
+            localStorage.setItem('cards', JSON.stringify([]));
+        }
+
+        setCards(JSON.parse(storageCards));
+    }, []);
+
+    return (
+        <div>
+            <div className="library-title">
+                {cards.length} / 365
+            </div>
+            <div className="library-cards">
+                {cards.map((card) => (
+                    <LibraryCard key={card.id} card={card} />
+                ))}
+            </div>
+        </div>
+    );
+};
+
+const LibraryCard = ({ card }) => {
+    return (
+        <div className="library-card">
+            <div className="library-card-image-container">
+                <img src={card.url} alt={card.description} />
+            </div>
+            <div className="library-card-date">{card.date}</div>
+            <div>{card.description}</div>
+        </div>
+    );
+};
